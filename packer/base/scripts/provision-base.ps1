5e3c70ba113e2ac952654892a042ab96b1ac4430
Nicolas Vo <VoN@corp.bellin.com>
2020-02-17 22:12:30 +0100

    Fix dotnet 3.5 installation

--- packer/base/scripts/provision-base.ps1
+++ packer/base/scripts/provision-base.ps1
@@ -6,8 +6,10 @@
 # Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0
 # Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\UnattendBackup\ActiveSetup\DisableFirstRunWizard" -Name DisableFirstRunWizard -Value 1
 
-Install-WindowsFeature NET-Framework-Core
-Install-WindowsFeature NET-Framework-Features
+DISM /Online /NoRestart /Enable-Feature /all /FeatureName:NetFx3ServerFeatures /Source:D:\sources\sxs
+DISM /Online /NoRestart /Enable-Feature /all /FeatureName:NetFx3 /Source:D:\sources\sxs
 Install-WindowsFeature NET-Framework-45-Features
 Install-WindowsFeature NET-Framework-45-Core
 Install-WindowsFeature NET-Framework-45-ASPNET
+# Install-WindowsFeature NET-Framework-Core
+# Install-WindowsFeature NET-Framework-Features
